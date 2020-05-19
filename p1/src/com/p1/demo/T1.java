78351f566adceba36fff87f038bad43e7fc3da90
zhujnan <247176894@qq.com>
2020-05-19 16:55:41 +0800

    1222
    Signed-off-by: zhujnan <247176894@qq.com>

--- p1/src/com/p1/demo/T1.java
+++ p1/src/com/p1/demo/T1.java
@@ -4,7 +4,7 @@ public class T1 {
 
 	public static void main(String[] args) {
 		System.out.println("helloworld2");
-
+		System.out.println("1234");
 	}
 
 }
--- p1/src/com/p1/demo/T2.java
+++ p1/src/com/p1/demo/T2.java
@@ -2,6 +2,6 @@ package com.p1.demo;
 
 public class T2 {
 	public static void main(String[] args) {
-		System.out.println("12344");
+		System.out.println();
 	}
 }
